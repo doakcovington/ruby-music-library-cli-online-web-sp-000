d8659ef990966e99bc13c25aa8ad8b7481292e7d
Doak Covington <doakcovington@protonmail.com>
2020-02-29 23:41:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,9 @@ class Song
 
   def initialize(name, artist = nil)
     @name = name
-    @artist = artist
+    if artist != nil
+      self.artist = artist
+    end
     save
   end
 
@@ -26,11 +28,6 @@ class Song
     song = self.new(name)
     song.save
     song
-  end
-
-  def artist=(artist)
-    @artist = artist
-    artist.songs << self unless artist.songs.include?(self) #pushes song to songs array if it doens't exist
   end
 
 end
