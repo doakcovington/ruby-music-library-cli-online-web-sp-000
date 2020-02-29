0693669f76969f37a7676f2a247aaa48f8a0b617
Doak Covington <doakcovington@protonmail.com>
2020-02-29 21:50:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,8 +28,8 @@ class Artist
   end
 
   def add_song(song)
-      song.artist = self unless song.artist
-      songs << song unless songs.include?(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
   end
 
 end
