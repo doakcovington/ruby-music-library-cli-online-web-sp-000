1498238c248b32aea813fa644df31d129ae8069e
Doak Covington <doakcovington@protonmail.com>
2020-03-01 22:50:22 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,10 @@
 module Concerns::Findable
 
+  module ClassMethods
+    def self.find_by_name(name)
+      #finds a song instance in @@all by the name of the property of the song
+      all.find{|instance| instance.name == name}
+    end
+  end#end of ClassMethods
 
 end
