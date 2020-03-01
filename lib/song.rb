f9b035d4ecab5a506acc6a60338b7b960c1b1c4b
Doak Covington <doakcovington@protonmail.com>
2020-03-01 22:22:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,7 +60,7 @@ class Song
   def self.find_or_create_by_name(name)
     #returns an existing song with the provided name if one exists in @@all
     #creates song if an existing match is not found
-    find_by_name(name) ||create(name)
+    find_by_name(name) || create(name)
   end
 
 
