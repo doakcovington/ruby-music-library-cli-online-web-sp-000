7d7a21904104fa036c0ba83f7d5348324ac4779e
Doak Covington <doakcovington@protonmail.com>
2020-03-01 22:29:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,6 +60,7 @@ class Song
     #returns an existing song with the provided name if one exists in @@all
     #creates song if an existing match is not found
     find_by_name(name) || create(name)
+    binding.pry
   end
 
 
