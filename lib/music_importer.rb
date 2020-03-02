06a3b046c7b5e4da0deb90503197fd9ac6d1d029
Doak Covington <doakcovington@protonmail.com>
2020-03-02 02:52:48 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -12,8 +12,8 @@ class MusicImporter
     Dir.glob("#{path}/*").map{|file| file.gsub("#{path}/", '')}
   end
 
-  def import(filename)
-    files << Song.create_from_filename(filename)
+  def import
+    files << Song.create_from_filename
   end
 
 end
